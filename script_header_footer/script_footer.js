7679f21d5236322c4610e618bbfab17832997371
Federico Sala <federico.sala01@liceobanfi.eu>
2023-03-19 16:48:09 +0100

    finale

---
+++ script_header_footer/script_footer.js
@@ -0,0 +1,7 @@
+document.write(`   <footer>
+<div class="foot">
+  <p>federico.sala01@liceobanfi.eu  -  gasparing.davide@liceobanfi.eu</p>
+  <p>+39 346 6433 325  -  +39 542 2655 886</p>
+  <p>Sala Federico  -  Gasparin Davide</p>
+</div>
+</footer>`)
